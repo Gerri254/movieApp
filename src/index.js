8962dccedb1f3a0829ed02a510e6bb1e389aa4e4
HP <luzzigeraldo@gmail.com>
2024-09-03 11:06:30 +0300

    Movie Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App';
+
+// Get the root element from your HTML
+const rootElement = document.getElementById('root');
+
+// Create a root using createRoot
+const root = ReactDOM.createRoot(rootElement);
+
+// Render your app with the new root
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
